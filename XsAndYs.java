39bfd618f6cf9190d0f539f97e79495adcff8f14
Willie Suggs <klbooker09@gmail.com>
2017-05-11 12:52:42 -0500

    Add files via upload

---
+++ XsAndYs.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xsandys;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class XsAndYs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("x" + "    " + "    y\n" + "______________" );
+        for(double x = -10; x <= 10; x += 0.5){
+            double y = x;
+            System.out.println(x + "    " + (y * y));
+            
+            }
+   
+    
+    
+    }
+    
+}
